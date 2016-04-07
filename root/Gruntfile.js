36357054325444807704709b4773bb5ad0b7394f
Andrew Dorokhov <andrew@dorohoff.net>
2016-04-08 00:19:36 +0300

    Initial template files.

---
+++ root/Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt) {
+    
+    // Project configuration:
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        
+        concat: {
+            options: {
+                stripBanners: true
+            },
+            dist: {
+                src: ['src/js/**/*.js'],
+                dest: 'dist/js/scripts.js'
+            }
+        },
+        
+        removelogging: {
+            dist: {
+                src: '<%= concat.dist.dest %>'
+            }
+        },
+        
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/js/scripts.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        
+        jshint: {
+            files: ['Gruntfile.js', 'src/js/**/*.js']
+        },
+        
+        less: {
+            dist: {
+                src: 'src/less/**/*.less',
+                dest: 'dist/css/styles.css'
+            }
+        },
+        
+        cssmin: {
+            dist: {
+                src: '<%= less.dist.dest %>',
+                dest: 'dist/css/styles.min.css'
+            }
+        },
+        
+        watch: {
+            js: {
+                files: 'src/js/**/*.js',
+                tasks: ['scripts']
+            },
+            less: {
+                files: 'src/less/**/*.less',
+                tasks: ['styles']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks("grunt-remove-logging");
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('styles', ['less', 'cssmin']);
+    grunt.registerTask('scripts', ['concat', 'removelogging', 'uglify', 'jshint']);
+    grunt.registerTask('default', ['styles', 'scripts', 'watch']);
+};
---
+++ template.js
@@ -0,0 +1,63 @@
+/*
+ * grunt-init-html5layout
+ * https://gruntjs.com/
+ *
+ * Copyright (c) 2016 Andrew Dorokhov
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+// Basic template description.
+exports.description = "Create a basic file structure for HTML5-layouts.";
+
+// Template-specific notes to be displayed after question prompts.
+exports.after = 'You should now install project dependencies with _npm ' +
+  'install_. After that, you may execute project tasks with _grunt_.';
+
+// Any existing file or directory matching this wildcard will cause a warning.
+exports.warnOn = '*';
+
+// The actual init template.
+exports.template = function(grunt, init, done) {
+	init.process({'version':'0.1.0'}, [
+		// Prompt for these values.
+		init.prompt('title', 'HTML5 Layout'),
+		init.prompt('name', 'html5-layout'),
+		init.prompt('description', 'HTML5 Layout for exclusive web project.'),
+		init.prompt('repository'),
+		init.prompt('author_name'),
+		init.prompt('author_email')
+	], function(err, props) {
+		// Files to copy (and process).
+		var files = init.filesToCopy(props);
+
+		// Actually copy (and process) files.
+		init.copyAndProcess(files, props);
+
+		// Generate package.json file, used by npm and grunt.
+		init.writePackageJSON('package.json', {
+			name: props.name,
+			version: props.version,
+			devDependencies: {
+				'grunt' : 'latest',
+                                'grunt-contrib-uglify' : 'latest',
+                                'grunt-contrib-concat' : 'latest',
+                                'grunt-contrib-jshint' : 'latest',
+                                'grunt-remove-logging' : 'latest',
+                                'grunt-contrib-less' : 'latest',
+                                'grunt-contrib-cssmin' : 'latest',
+                                'grunt-contrib-watch' : 'latest'
+			}
+		});
+                
+                var join = require("path").join;
+                grunt.file.mkdir( join(init.destpath(), 'src/js') );
+                grunt.file.mkdir( join(init.destpath(), 'src/less') );
+                grunt.file.mkdir( join(init.destpath(), 'src/img') );
+                grunt.file.mkdir( join(init.destpath(), 'dist') );
+
+		// All done!
+    		done();
+	});
+}
